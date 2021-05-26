d03cb3e1cdf70ad36be21bf0bd8815a9d62d36b4
legend380 <81872640+legend380@users.noreply.github.com>
2021-05-26 23:14:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 console.log("Bot coded by Felix_Playz#1000");
+const express = require("express")
+const app = express ()
+
+app.get("/", (req, res) => {
+  res.send("hey ya")
+})
+
+app.listen(3000, () => { 
+  console.log("Project is ready")
+})
 
 //====================================================================================CONSTANTS REQUIRED ON READY=============================================================================================
 const { Client, Collection } = require('discord.js');
